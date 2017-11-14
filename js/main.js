7aba71a12511ff4fa7eb43fd687320141b9be89d
Daniel Chen <danielchenphhs@gmail.com>
2017-11-14 16:08:07 -0600

    set up maps

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-d3.squareMap.render("data/test.csv","#vis");
+var map1 = d3.squareMap.setAttr({
+    colorSet: 'Reds',
+    labelStyle: 'abbr'
+}).render("data/gunspercapita.csv","#vis1");
+var map = d3.squareMap.setAttr({
+    colorSet: 'Greens',
+    labelStyle: 'abbr'
+}).render("data/gundeathspercapita.csv","#vis2");
