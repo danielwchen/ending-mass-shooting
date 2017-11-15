96e174791c968eaab7f84245bc682271dca7f949
Daniel Chen <danielchenphhs@gmail.com>
2017-11-15 13:56:22 -0600

    making legend

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@
 //     colorNumber: 6,
 
 // }).render("data/gundeathspercapita.csv","#vis2");
+
+var mapScale = d3.scaleThreshold().domain([0,10,20,100])
 var map1 = d3.squareMap.setAttr({
     labelStyle: 'abbr',
-    scale: d3.scaleThreshold().domain([0,10,20,100]),
+    scale: mapScale,
     legend: "Firearms per 1,000 people")
 }).render("data/gunspercapita.csv","#vis1");
